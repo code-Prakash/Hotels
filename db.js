14e058740ef407d7b8475be22e154e105934315e
code-Prakash <prakashkumar959903@gmail.com>
2025-09-07 23:05:09 +0530

    debug db.js

--- db.js
+++ db.js
@@ -6,10 +6,7 @@ require('dotenv').config();
 const mongoURL = process.env.MONGODB_URL;
 
 //Setup Mongoose connection
-mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(mongoURL);
 
 //Get the default conection
 //Mongoose maintain a default connection object representing the MongoDB connection
